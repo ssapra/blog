086f0d008591bb49c10490f414644750e8e775da
Shaan Sapra <ssapra@uchicago.edu>
2014-05-23 16:48:37 -0500

    Finished the Golden Seven

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,42 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by_id(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to posts_path
+    # redirect_to post_path(id: post.id)
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def update
+    post = Post.find_by_id(params[:id])
+    # retrieve
+
+    # update
+    post.update_attributes(post_params)
+
+    # redirect
+    redirect_to post_path(id: post.id)
+  end
+
+  def destroy
+    post = Post.find_by_id(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author, :published_at, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Blog::Application.routes.draw do
   get "/" => 'home#index'
 
+  get "/posts/new" => 'posts#new'
+  get "/posts/:id/edit" => 'posts#edit'
   get "/posts" => 'posts#index'
   get "/posts/:id" => 'posts#show', as: :post
+  patch "/posts/:id" => 'posts#update'
+  delete "/posts/:id" => 'posts#destroy'
+  post "/posts" => 'posts#create'
+
 end
